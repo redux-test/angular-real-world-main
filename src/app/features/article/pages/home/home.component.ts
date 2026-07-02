8a6b5535609962b257c605779cf84e3946ddcf18
redux-test <redux-github-test@karsun-llc.com>
2026-07-02 12:27:26 -0400

    fix(home): Fix race condition in HomeComponent authentication state subscription and ensure change detection triggers [WTP-14]

--- src/app/features/article/pages/home/home.component.ts
+++ src/app/features/article/pages/home/home.component.ts
@@ -43,19 +43,15 @@ export default class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.userService.isAuthenticated
-      .pipe(
-        tap((isAuthenticated) => {
-          if (isAuthenticated) {
-            this.setListTo("feed");
-          } else {
-            this.setListTo("all");
-          }
-        }),
-        takeUntilDestroyed(this.destroyRef),
-      )
-      .subscribe(
-        (isAuthenticated: boolean) => (this.isAuthenticated = isAuthenticated),
-      );
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((isAuthenticated: boolean) => {
+        this.isAuthenticated = isAuthenticated;
+        if (isAuthenticated) {
+          this.setListTo("feed");
+        } else {
+          this.setListTo("all");
+        }
+      });
   }
 
   setListTo(type: string = "", filters: Object = {}): void {
@@ -66,6 +62,6 @@ export default class HomeComponent implements OnInit {
     }
 
     // Otherwise, set the list object
-    this.listConfig = { type: type, filters: filters };
+    this.listConfig = { type: type, filters: { ...filters } };
   }
 }
